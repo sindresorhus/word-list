de09cf2d889c99255dfe2894b0b496aa6a35b22b
Sindre Sorhus <sindresorhus@gmail.com>
2014-05-16 13:45:24 +0200

    use `got` for the word fetching

--- fetch-words.js
+++ fetch-words.js
@@ -1,12 +1,12 @@
 'use strict';
 var fs = require('fs');
-var request = require('request');
+var got = require('got');
 var url = 'https://raw.github.com/atebits/Words/master/Words/en.txt';
 
-request(url, function (err, res, body) {
-	if (err || res.statusCode !== 200) {
-		throw new Error(error || response.statusCode);
+got(url, function (err, res) {
+	if (err) {
+		throw new Error(err);
 	}
 
-	fs.writeFileSync('words.txt', body.trim());
+	fs.writeFileSync('words.txt', res.trim());
 });
