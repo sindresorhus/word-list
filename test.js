ee4bd99b1a67651d8068ec2181160ef259dc196f
Sindre Sorhus <sindresorhus@gmail.com>
2014-09-05 14:19:43 +0200

    minor tweak

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 var fs = require('fs');
 var assert = require('assert');
-var wordList = require('./index');
+var wordList = require('./');
 
 it('should return list of words', function () {
 	assert(wordList.length > 0);
