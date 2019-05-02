17ca93e0eba555241c5916b385e3c61e934893be
Yanis Benson <yanis.benson@gmail.com>
2019-05-03 01:42:50 +0300

    added bad word filters, fixes #2

--- fetch-words.js
+++ fetch-words.js
@@ -4,9 +4,53 @@ const got = require('got');
 
 const url = 'https://raw.githubusercontent.com/atebits/Words/master/Words/en.txt';
 
+const badWords = require('badwords-list').array;
+const leoProfanity = require('leo-profanity');
+
+leoProfanity.loadDictionary('en');
+
+const badWordsSet = new Set(badWords);
+const badWordsExclusions = new Set(['ass', 'asses', 'anal', 'cum', 'tit', 'bum', 'hore', 'sex', 'cipa', 'butt', 'cock', 'homo', 'eros', 'cox', 'semen', 'damn', 'jap', 'lust', 'hell', 'pron', 'crap']);
+const badWordsWithExclusions = badWords.filter(word => !badWordsExclusions.has(word));
+
+const filters = [
+	{
+		name: 'leo-profanity',
+		fn: word => !leoProfanity.check(word)
+	},
+	{
+		name: 'badwords-list exact',
+		fn: word => !badWordsSet.has(word)
+	},
+	{
+		name: 'badwords-list with exclusions substring',
+		fn: word => {
+			for (const badWord of badWordsWithExclusions) {
+				if (word.indexOf(badWord) !== -1) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+];
+
 (async () => {
 	const {body} = await got(url);
-	fs.writeFileSync('words.txt', body.trim());
+	let words = body.trim().split('\n');
+
+	const originalLength = words.length;
+
+	for (const filter of filters) {
+		const previousLength = words.length;
+		words = words.filter(filter.fn);
+		console.log(`filtered ${previousLength - words.length} bad words with filter '${filter.name}'`);
+	}
+
+	console.log(`filtered ${originalLength - words.length} bad words total`);
+
+	fs.writeFileSync('words.txt', words.join('\n'));
 })().catch(error => {
 	console.error(error);
 	process.exit(1); // eslint-disable-line unicorn/no-process-exit
--- test.js
+++ test.js
@@ -2,7 +2,47 @@ import fs from 'fs';
 import test from 'ava';
 import wordList from '.';
 
+const badWords = [
+	'bumfuck',
+	'bumfucks',
+	'ass'
+];
+
+const goodWords = [
+	'accumulator',
+	'advertisement',
+	'album',
+	'assassination',
+	'cryptanalyst',
+	'anticipation',
+	'antisexual',
+	'antiterrorism',
+	'bumps',
+	'button',
+	'chores',
+	'cockiness',
+	'damnable',
+	'japan',
+	'illustrator',
+	'shell',
+	'pronoun',
+	'scrap',
+];
+
 test('main', t => {
 	t.true(wordList.length > 0);
 	t.true(fs.statSync(wordList).size > 1000);
+});
+
+test('bad words, #2', t => {
+	const wordListText = fs.readFileSync(wordList, 'utf8');
+	const words = wordListText.split('\n');
+
+	for (const badWord of badWords) {
+		t.true(words.indexOf(badWord) === -1, badWord);
+	}
+
+	for (const goodWord of goodWords) {
+		t.true(words.indexOf(goodWord) !== -1, goodWord);
+	}
 });
