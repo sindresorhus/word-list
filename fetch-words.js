633046da0f6d0226db878d378f4f2a86775cfd6f
Yanis Benson <yanis.benson@gmail.com>
2019-06-19 22:31:45 +0300

    Filter out bad words (#5)

--- fetch-words.js
+++ fetch-words.js
@@ -2,11 +2,61 @@
 const fs = require('fs');
 const got = require('got');
 
+const verbose = process.argv.includes('-v');
+
+const {array: badWords} = require('badwords-list');
+const leoProfanity = require('leo-profanity');
+
+leoProfanity.loadDictionary('en');
+
+const badWordsSet = new Set(badWords);
+
+const ourBadWordsSet = new Set([
+	'bumfuck',
+	'bumfucks',
+	'tittie'
+]);
+
 const url = 'https://raw.githubusercontent.com/atebits/Words/master/Words/en.txt';
+
+const filters = [
+	{
+		name: 'leo-profanity',
+		getReason: word => leoProfanity.check(word) ? 'is marked bad by leo-profanity' : undefined
+	},
+	{
+		name: 'badwords-list exact',
+		getReason: word => badWordsSet.has(word) ? 'is in badwords-list' : undefined
+	},
+	{
+		name: 'our bad words list',
+		getReason: word => ourBadWordsSet.has(word) ? 'is in our bad words list' : undefined
+	}
+];
 
 (async () => {
 	const {body} = await got(url);
-	fs.writeFileSync('words.txt', body.trim());
+	let words = body.trim().split('\n');
+
+	const originalLength = words.length;
+
+	for (const filter of filters) {
+		const previousLength = words.length;
+		words = words.filter(word => {
+			const reason = filter.getReason(word);
+
+			if (reason !== undefined && verbose) {
+				console.log(`word "${word}" excluded because it ${reason}`);
+			}
+
+			return reason === undefined;
+		});
+		console.log(`filtered ${previousLength - words.length} bad words with filter '${filter.name}'`);
+	}
+
+	console.log(`filtered ${originalLength - words.length} bad words total out of ${originalLength} original words`);
+
+	fs.writeFileSync('words.txt', words.join('\n'));
 })().catch(error => {
 	console.error(error);
 	process.exit(1); // eslint-disable-line unicorn/no-process-exit
--- test.js
+++ test.js
@@ -2,7 +2,49 @@ import fs from 'fs';
 import test from 'ava';
 import wordList from '.';
 
+const badWords = [
+	'bumfuck',
+	'bumfucks',
+	'ass'
+];
+
+const goodWords = [
+	'accumulator',
+	'advertisement',
+	'album',
+	'assassination',
+	'cryptanalyst',
+	'anticipation',
+	'antisexual',
+	'antiterrorism',
+	'bumps',
+	'button',
+	'chores',
+	'cockiness',
+	'damnable',
+	'japan',
+	'illustrator',
+	'shell',
+	'pronoun',
+	'scrap',
+	'aerospace',
+	'backspace'
+];
+
 test('main', t => {
 	t.true(wordList.length > 0);
 	t.true(fs.statSync(wordList).size > 1000);
+});
+
+test('bad words, #2', t => {
+	const wordListText = fs.readFileSync(wordList, 'utf8');
+	const words = wordListText.split('\n');
+
+	for (const badWord of badWords) {
+		t.false(words.includes(badWord), badWord);
+	}
+
+	for (const goodWord of goodWords) {
+		t.true(words.includes(goodWord), goodWord);
+	}
 });
